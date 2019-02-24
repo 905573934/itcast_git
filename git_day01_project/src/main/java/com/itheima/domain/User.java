411b5d1a17dbac0a492f831cee645568857eb337
123 <123>
2019-02-24 17:43:11 +0800

    新建User.java，提交到本地仓库1

--- git_day01_project/src/main/java/com/itheima/domain/User.java
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -3,7 +3,15 @@ package com.itheima.domain;
 public class User {
     private Integer id;
     private String name;
+    private String name1;
 
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
 
     @Override
     public String toString() {
