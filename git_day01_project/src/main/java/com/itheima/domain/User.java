a0f258fc124a49f95d7a5d78d3cd827dd048dd38
123 <123>
2019-02-24 19:29:23 +0800

    修改User.java，提交到本地仓库1

--- git_day01_project/src/main/java/com/itheima/domain/User.java
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -4,14 +4,8 @@ public class User {
     private Integer id;
     private String name;
     private String name1;
+    private String name2;
 
-    public String getName1() {
-        return name1;
-    }
-
-    public void setName1(String name1) {
-        this.name1 = name1;
-    }
 
     @Override
     public String toString() {
@@ -35,5 +29,21 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
     }
 }
